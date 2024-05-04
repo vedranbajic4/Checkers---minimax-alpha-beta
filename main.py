e882f2cb2e303fd947888dc52bc528b1d362827a
Vedran Bajic <bajic196@gmail.com>
2024-05-04 15:05:17 +0200

    Skoro zavrsen, ali init commit

---
+++ main.py
@@ -0,0 +1,262 @@
+import pygame
+import playing
+
+# Initialize Pygame
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 600, 600
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH // COLS
+PADDING = 5
+CHIP_SIZE = WIDTH // COLS - 2 * PADDING
+BORDER_WIDTH = 4
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 236, 161)
+INF = 100000000
+# Tokens
+RED_CHIP = pygame.image.load("images/red_chip.png")
+BLACK_CHIP = pygame.image.load("images/black_chip.png")
+BLACK_CHIP2 = pygame.image.load("images/black_chip2.png")
+RED_CHIP2 = pygame.image.load("images/red_chip2.png")
+
+RED_CHIP = pygame.transform.scale(RED_CHIP, (SQUARE_SIZE - 2 * PADDING, SQUARE_SIZE - 2 * PADDING))
+RED_CHIP2 = pygame.transform.scale(RED_CHIP2, (SQUARE_SIZE - 2 * PADDING, SQUARE_SIZE - 2 * PADDING))
+BLACK_CHIP = pygame.transform.scale(BLACK_CHIP, (SQUARE_SIZE - 2 * PADDING, SQUARE_SIZE - 2 * PADDING))
+BLACK_CHIP2 = pygame.transform.scale(BLACK_CHIP2, (SQUARE_SIZE - 2 * PADDING, SQUARE_SIZE - 2 * PADDING))
+
+# Create the display surface
+turn_played = False
+moves = []
+opcija = '1'
+ima_jedenja = False
+# Checkers board representation, 1=red, 2=black
+
+board = [
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 0, 2, 0, 2, 0, 2, 0],
+    [0, 2, 0, 2, 0, 2, 0, 2],
+    [2, 0, 2, 0, 2, 0, 2, 0]
+]
+next_move = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+
+# Main function to draw the board
+def draw_board():
+    screen.fill(WHITE)
+    for row in range(ROWS):
+        for col in range(COLS):
+            if (row + col) % 2 == 1:
+                pygame.draw.rect(screen, BLACK, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+    for r in range(ROWS):
+        for c in range(COLS):
+            if board[r][c] == 1:
+                screen.blit(RED_CHIP, (c * SQUARE_SIZE + PADDING, r * SQUARE_SIZE + PADDING))
+            if board[r][c] == 2:
+                screen.blit(BLACK_CHIP, (c * SQUARE_SIZE + PADDING, r * SQUARE_SIZE + PADDING))
+            if board[r][c] == 3:
+                screen.blit(RED_CHIP2, (c * SQUARE_SIZE + PADDING, r * SQUARE_SIZE + PADDING))
+            if board[r][c] == 4:
+                screen.blit(BLACK_CHIP2, (c * SQUARE_SIZE + PADDING, r * SQUARE_SIZE + PADDING))
+
+            if next_move[r][c] == 1:
+                pygame.draw.rect(screen, RED, (c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE),
+                                 BORDER_WIDTH)
+            if next_move[r][c] == 2:
+                pygame.draw.rect(screen, YELLOW, (c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE),
+                                 BORDER_WIDTH)
+            if next_move[r][c] == 3:
+                pygame.draw.rect(screen, YELLOW, (c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE),
+                                 BORDER_WIDTH//2)
+
+
+def highlight(r, c):    # ovo zapravo napravi moves za trenutno kliknuto polje
+    global moves, ima_jedenja
+    if board[r][c] != 0 and board[r][c] != 1 and board[r][c] != 3:
+        next_move[r][c] = 2
+        moves = []
+        playing.find_moves(board, r, c, 2, [], True, moves)
+        #print("Za trenutno kliknuto polje dostupni potezi su:")
+        for move in moves:
+            if ima_jedenja and (abs(move[0][0] - r) + abs(move[0][1] - c)) == 4 and opcija == '1':
+                next_move[move[-1][0]][move[-1][1]] = 1
+                next_move[r][c] = 3
+                #print(move)
+            if opcija == '2':
+                next_move[move[-1][0]][move[-1][1]] = 1
+            if opcija == '1' and not ima_jedenja:
+                next_move[move[-1][0]][move[-1][1]] = 1
+                #print(move)
+        #playing.print_board(next_move)
+
+
+def make_turn(r, c):  # kliknuto je na R, C
+    global turn_played, moves, ima_jedenja
+    if next_move[r][c] != 1:
+        return False
+
+    ima_jedenja = False
+    x = ru = cu = 1
+    #print("Igram potez")
+    for i in range(8):
+        for j in range(8):
+            if next_move[i][j] == 2 or next_move[i][j] == 3:
+                x = board[i][j]
+                ru = i
+                cu = j
+                break
+    #print("Pocinjem sa polja: ", ru, " ", cu)
+    for move in moves:
+        if move[-1][0] == r and move[-1][1] == c:
+            board[ru][cu] = 0
+            for elem in move:
+                board[ru + (elem[0] - ru) // 2][cu + (elem[1] - cu) // 2] = 0
+                ru = elem[0]
+                cu = elem[1]
+
+            board[r][c] = x
+
+            if r == 0:
+                board[r][c] = 4
+
+            turn_played = True
+            return True
+
+
+def handle_click(pos):
+    global next_move
+    r = pos[1] // SQUARE_SIZE
+    c = pos[0] // SQUARE_SIZE
+    print("Clicked on:", r, c)
+    #playing.print_board(next_move)
+
+    if make_turn(r, c):
+        for i in range(8):
+            for j in range(8):
+                next_move[i][j] = 0
+    else:
+        for i in range(8):
+            for j in range(8):
+                if next_move[i][j] != 3:
+                    next_move[i][j] = 0
+        highlight(r, c)
+
+
+def find_all_moves():
+    if opcija == '2':
+        return
+    global ima_jedenja
+    for r in range(8):
+        for c in range(8):
+            if board[r][c] == 2 or board[r][c] == 4:
+                moves = []
+                playing.find_moves(board, r, c, 2, [], True, moves)
+                for move in moves:
+                    if abs(move[0][0] - r) + abs(move[0][1] - c) == 4:
+                        ima_jedenja = True
+                        next_move[r][c] = 3
+                        break
+
+
+def main():         # Main game loop
+    global board, turn_played
+    board = [
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2],
+        [2, 0, 2, 0, 2, 0, 2, 0]
+    ]
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                handle_click(pos)
+
+        draw_board()
+        pygame.display.update()
+
+        if turn_played:
+            playing.minimax(board, 7, 7, True, 1, -INF, INF, opcija)
+            turn_played = False
+            find_all_moves()
+
+
+if __name__ == "__main__":
+    '''while True:
+        opcija = input("Da li su potezi jedenja obavezni? 1 da, 2 ne: ")
+        if opcija == '1' or opcija == '2':
+            break'''
+    opcija = '2'
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Checkers")
+    main()
+'''
+    board = [
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 2, 0, 0, 0],
+        [0, 2, 0, 0, 0, 0, 0, 1],
+        [0, 0, 2, 0, 2, 0, 1, 0],
+        [0, 0, 0, 2, 0, 0, 0, 0],
+        [0, 0, 2, 0, 2, 0, 2, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [2, 0, 3, 0, 0, 0, 0, 0]
+    ]
+    board1 = [
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 2, 0, 0, 0],
+        [0, 2, 0, 0, 0, 0, 0, 1],
+        [0, 0, 2, 0, 2, 0, 0, 0],
+        [0, 0, 0, 2, 0, 1, 0, 0],
+        [0, 0, 2, 0, 2, 0, 2, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [2, 0, 3, 0, 0, 0, 0, 0]
+    ]
+    board2 = [
+        [0, 1, 0, 1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 0, 0, 0, 0, 0, 1],
+        [0, 0, 2, 0, 0, 0, 1, 0],
+        [0, 0, 0, 2, 0, 0, 0, 0],
+        [0, 0, 2, 0, 2, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [2, 0, 3, 0, 0, 0, 0, 0]
+    ]
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit(0)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                playing.minimax(board, 5, 5, True, 1, -INF, INF)
+
+        draw_board()
+        pygame.display.update()'''
+
---
+++ playing.py
@@ -0,0 +1,335 @@
+DAMA = 40
+INF = 100000000
+
+moves = []
+value_dame = [0, 50, 90, 120, 145, 167, 187, 207, 220, 233, 246, 255, 260]
+value_chipa = [0, 30, 60, 90, 120, 140, 160, 180, 200, 215, 230, 240, 250]
+dikt = {}
+
+def in_bounds(r, c):
+    return 7 >= r >= 0 and 7 >= c >= 0
+
+
+def opposite_player(p):
+    if p == 1:
+        return 2
+    else:
+        return 1
+
+
+def state_of_board(board):  # True ako je neko pobedio
+    jed = dva = 0
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] == 1 or board[i][j] == 3:
+                jed += 1
+            elif board[i][j] == 2 or board[i][j] == 4:
+                dva += 1
+    return jed == 0 or dva == 0
+
+
+def onion_rings(r):
+    if r == 0 or r == 7:
+        return 30
+    return 0
+
+
+def print_board(board):
+    print("Board:")
+    for red in board:
+        print(red)
+    print("\n")
+
+
+def value_of_board(board):  # Ako je sto vece 2 pobedjuje, sto manje 1
+    jed = dva = 0
+    jed_d = dva_d = 0
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] == 1:
+                jed += 1
+            elif board[i][j] == 2:
+                dva += 1
+            if board[i][j] == 3:
+                jed_d += 1
+            if board[i][j] == 4:
+                dva_d += 1
+
+    score = 0
+    score += value_chipa[dva]
+    score += value_dame[dva_d]
+    score -= value_chipa[jed]
+    score -= value_dame[jed_d]
+
+    for r in range(8):
+        for c in range(8):
+            if board[r][c] == 1 or board[r][c] == 3:
+                score -= onion_rings(r)
+            if board[r][c] == 2 or board[r][c] == 4:
+                score += onion_rings(r)
+            if board[r][c] == 3:
+                score -= DAMA
+            if board[r][c] == 4:
+                score += DAMA
+    return (-1)*score
+
+
+def find_moves(board, r, c, igrac, dosad, prvi_potez, moves):
+    if igrac == 1:
+        if prvi_potez:
+            if in_bounds(r + 1, c + 1) and board[r + 1][c + 1] == 0:
+                moves.append([(r + 1, c + 1)])
+
+            if in_bounds(r + 1, c - 1) and board[r + 1][c - 1] == 0:
+                moves.append([(r + 1, c - 1)])
+
+        if in_bounds(r + 2, c + 2) and (board[r + 1][c + 1] == 2 or board[r + 1][c + 1] == 4) and board[r + 2][c + 2] == 0:
+            novi_dosad = dosad.copy()
+            novi_dosad.append((r + 2, c + 2))
+            moves.append(novi_dosad)
+
+            pojeden = board[r + 1][c + 1]
+            board[r + 1][c + 1] = 0
+            board[r][c], board[r + 2][c + 2] = board[r + 2][c + 2], board[r][c]
+
+            find_moves(board, r + 2, c + 2, igrac, novi_dosad, False, moves)
+
+            board[r + 1][c + 1] = pojeden
+            board[r][c], board[r + 2][c + 2] = board[r + 2][c + 2], board[r][c]
+
+        if in_bounds(r+2, c-2) and (board[r+1][c-1] == 2 or board[r+1][c-1] == 4) and board[r+2][c-2] == 0:
+            novi_dosad = dosad.copy()
+            novi_dosad.append((r + 2, c - 2))
+            moves.append(novi_dosad)
+
+            pojeden = board[r + 1][c - 1]
+            board[r + 1][c - 1] = 0
+            board[r][c], board[r + 2][c - 2] = board[r + 2][c - 2], board[r][c]
+
+            find_moves(board, r + 2, c - 2, igrac, novi_dosad, False, moves)
+
+            board[r + 1][c - 1] = pojeden
+            board[r][c], board[r + 2][c - 2] = board[r + 2][c - 2], board[r][c]
+    if igrac == 2:
+        if prvi_potez:
+            if in_bounds(r - 1, c - 1) and board[r - 1][c - 1] == 0:
+                moves.append([(r - 1, c - 1)])
+
+            if in_bounds(r - 1, c + 1) and board[r - 1][c + 1] == 0:
+                moves.append([(r - 1, c + 1)])
+
+        if in_bounds(r - 2, c - 2) and (board[r - 1][c - 1] == 1 or board[r - 1][c - 1] == 3) and board[r - 2][c - 2] == 0:
+            novi_dosad = dosad.copy()
+            novi_dosad.append((r - 2, c - 2))
+            moves.append(novi_dosad)
+
+            pojeden = board[r - 1][c - 1]
+            board[r - 1][c - 1] = 0
+            board[r - 2][c - 2], board[r][c] = board[r][c], board[r - 2][c - 2]
+
+            find_moves(board, r - 2, c - 2, igrac, novi_dosad, False, moves)
+
+            board[r - 1][c - 1] = pojeden
+            board[r - 2][c - 2], board[r][c] = board[r][c], board[r - 2][c - 2]
+
+        if in_bounds(r - 2, c + 2) and (board[r - 1][c + 1] == 1 or board[r - 1][c + 1] == 3) and board[r - 2][c + 2] == 0:
+            novi_dosad = dosad.copy()
+            novi_dosad.append((r - 2, c + 2))
+            moves.append(novi_dosad)
+
+            pojeden = board[r - 1][c + 1]
+            board[r - 1][c + 1] = 0
+            board[r - 2][c + 2], board[r][c] = board[r][c], board[r - 2][c + 2]
+
+            find_moves(board, r - 2, c + 2, igrac, novi_dosad, False, moves)
+
+            board[r - 1][c + 1] = pojeden
+            board[r - 2][c + 2], board[r][c] = board[r][c], board[r - 2][c + 2]
+
+    if board[r][c] == 3:
+        if prvi_potez:
+            if in_bounds(r - 1, c - 1) and board[r - 1][c - 1] == 0:
+                moves.append([(r - 1, c - 1)])
+
+            if in_bounds(r - 1, c + 1) and board[r - 1][c + 1] == 0:
+                moves.append([(r - 1, c + 1)])
+
+        if in_bounds(r - 2, c - 2) and (board[r - 1][c - 1] == 2 or board[r - 1][c - 1] == 4) and board[r - 2][c - 2] == 0:
+            novi_dosad = dosad.copy()
+            novi_dosad.append((r - 2, c - 2))
+            moves.append(novi_dosad)
+
+            pojeden = board[r - 1][c - 1]
+            board[r - 1][c - 1] = 0
+            board[r - 2][c - 2],  board[r][c] = board[r][c], board[r - 2][c - 2]
+
+            find_moves(board, r - 2, c - 2, igrac, novi_dosad, False, moves)
+
+            board[r - 1][c - 1] = pojeden
+            board[r - 2][c - 2],  board[r][c] = board[r][c], board[r - 2][c - 2]
+
+        if in_bounds(r - 2, c + 2) and (board[r - 1][c + 1] == 2 or board[r - 1][c + 1] == 4) and board[r - 2][c + 2] == 0:
+            novi_dosad = dosad.copy()
+            novi_dosad.append((r - 2, c + 2))
+            moves.append(novi_dosad)
+
+            pojeden = board[r - 1][c + 1]
+            board[r - 1][c + 1] = 0
+            board[r][c], board[r - 2][c + 2] = board[r - 2][c + 2], board[r][c]
+
+            find_moves(board, r - 2, c + 2, igrac, novi_dosad, False, moves)
+
+            board[r - 1][c + 1] = pojeden
+            board[r][c], board[r - 2][c + 2] = board[r - 2][c + 2], board[r][c]
+    if board[r][c] == 4:
+        if prvi_potez:
+            if in_bounds(r + 1, c + 1) and board[r + 1][c + 1] == 0:
+                moves.append([(r + 1, c + 1)])
+
+            if in_bounds(r + 1, c - 1) and board[r + 1][c - 1] == 0:
+                moves.append([(r + 1, c - 1)])
+
+        if in_bounds(r + 2, c + 2) and (board[r + 1][c + 1] == 1 or board[r + 1][c + 1] == 3) and board[r + 2][c + 2] == 0:
+            novi_dosad = dosad.copy()
+            novi_dosad.append((r + 2, c + 2))
+            moves.append(novi_dosad)
+
+            pojeden = board[r + 1][c + 1]
+            board[r + 1][c + 1] = 0
+            board[r][c], board[r + 2][c + 2] = board[r + 2][c + 2], board[r][c]
+
+            find_moves(board, r + 2, c + 2, igrac, novi_dosad, False, moves)
+
+            board[r + 1][c + 1] = pojeden
+            board[r][c], board[r + 2][c + 2] = board[r + 2][c + 2], board[r][c]
+
+        if in_bounds(r+2, c-2) and (board[r+1][c-1] == 1 or board[r+1][c-1] == 3) and board[r+2][c-2] == 0:
+            novi_dosad = dosad.copy()
+            novi_dosad.append((r + 2, c - 2))
+            moves.append(novi_dosad)
+
+            pojeden = board[r + 1][c - 1]
+            board[r + 1][c - 1] = 0
+            board[r][c], board[r + 2][c - 2] = board[r + 2][c - 2], board[r][c]
+
+            find_moves(board, r + 2, c - 2, igrac, novi_dosad, False, moves)
+
+            board[r + 1][c - 1] = pojeden
+            board[r][c], board[r + 2][c - 2] = board[r + 2][c - 2], board[r][c]
+
+
+def play_move(potez, board, pojed):
+    r = potez[0][0]
+    c = potez[0][1]
+
+    pojed.append((r, c, board[r][c]))
+
+    for tup in range(1, len(potez)):
+        r2 = potez[tup][0]
+        c2 = potez[tup][1]
+
+        if (abs(r2 - r) + abs(c2 - c)) == 4:
+            pojed.append((r+(r2-r)//2, c+(c2-c)//2, board[r+(r2-r)//2][c+(c2-c)//2]))
+            board[r+(r2-r)//2][c+(c2-c)//2] = 0   # uklanjam pojedenog
+
+        pojed.append((r2, c2, board[r2][c2]))
+        board[r2][c2] = board[r][c]
+
+        board[r][c] = 0
+        r = r2
+        c = c2
+    if (r == 0 or r == 7) and board[r][c] <= 2:
+        board[r][c] += 2
+
+
+def restore_board(board, pojed):
+    for tup in pojed:
+        board[tup[0]][tup[1]] = tup[2]
+
+
+def hash_board(board):
+    ret = ""
+    for r in range(8):
+        for c in range(8):
+            if board[r][c] == 0:
+                ret = "0" + ret
+            if board[r][c] == 1:
+                ret = "1" + ret
+            if board[r][c] == 2:
+                ret = "2" + ret
+            if board[r][c] == 3:
+                ret = "3" + ret
+            if board[r][c] == 4:
+                ret = "4" + ret
+
+    return ret
+
+
+def minimax(board, depth, max_depth, maximizing_player, player, alpha, beta, opcija):    # depth je dubina, mp True/False, player = 1 ili 2
+    if depth == 0 or state_of_board(board):
+        return value_of_board(board)
+    hash_string = hash_board(board)
+    if hash_string in dikt:
+        if depth == max_depth:
+            pojeden = []
+            play_move(dikt[hash_string][0], board, pojeden)
+        return dikt[hash_string][1]
+
+    global moves
+    potezi = []
+    for r in range(8):
+        for c in range(8):
+            if player == 1 and (board[r][c] == 1 or board[r][c] == 3):
+                moves = []
+                find_moves(board, r, c, player, [], True, moves)
+                for move in moves:
+                    potezi.append([(r, c)] + move)
+            if player == 2 and (board[r][c] == 2 or board[r][c] == 4):
+                moves = []
+                find_moves(board, r, c, player, [], True, moves)
+                for move in moves:
+                    potezi.append([(r, c)] + move)
+
+    ima_jedenja = False
+    for potez in potezi:
+        if abs(potez[0][0]-potez[1][0]) + abs(potez[0][1]-potez[1][1]) == 4:
+            ima_jedenja = True
+            break
+
+    new_board = board.copy()
+    best_potez = []
+    if maximizing_player:
+        value = -INF
+    else:
+        value = INF
+
+    for potez in potezi:
+        if ima_jedenja and opcija == '1' and (abs(potez[0][0] - potez[1][0]) + abs(potez[0][1] - potez[1][1]) == 2):
+            continue    # preskacem potez koji nema jedenja u sebi
+        pojeden = []
+        play_move(potez, board, pojeden)
+
+        tren_value = minimax(new_board, depth-1, max_depth, not maximizing_player, opposite_player(player), alpha, beta, opcija)
+
+        if maximizing_player:
+            if tren_value > value:
+                value = tren_value
+                best_potez = potez
+            alpha = max(alpha, value)
+        else:
+            if tren_value < value:
+                value = tren_value
+                best_potez = potez
+            beta = min(beta, value)
+
+        restore_board(new_board, pojeden)
+        if beta <= alpha:
+            break
+
+    dikt[hash_string] = (best_potez, value)
+
+    if depth == max_depth:
+        pojeden = []
+        play_move(best_potez, board, pojeden)
+
+    return value
